ad2597f00366c381e4bf7c40d459ffc90d2d2bce
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-16 20:50:45 -0700

    lecture for selenium

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,20 +1,42 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 import java.time.Duration;
 
 public class LoginTests {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginEmptyEmailPasswordTest () throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("demo@class.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+        //
+        WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+
+
+        Thread.sleep(5000);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
