6128eb04bfe0589dcfaf818a4583acf11f83a172
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-20 22:58:19 -0700

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -21,6 +21,68 @@ public class LoginTests {
 
         driver.quit();
     }
+    @Test
+    public static void searchSongbyTrangCode () throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/#!/home";
+            driver.get(url);
+
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("demo@class.com");
+
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+            passwordField.click();
+            passwordField.sendKeys("te$t$tudent");
+
+
+            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+            submitButton.click();
+            //
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+
+            //use Queue
+            WebElement queueLink = driver.findElement(By.cssSelector("[href=\"#!/queue\"]"));
+            //Assert.assertTrue(avatarIcon.isDisplayed());
+            queueLink.click();
+
+            WebElement queueContainer = driver.findElement(By.id("queueWrapper"));
+            Assert.assertTrue(queueContainer.isDisplayed());
+
+
+
+            Thread.sleep(8000);
+
+
+
+//            //there is two clas name home so we use [href="#!/home"]
+//            WebElement homeLink = driver.findElement(By.cssSelector("[href=\"#!/home\"]"));
+//            //Assert.assertTrue(avatarIcon.isDisplayed());
+//            homeLink.click();
+//            //there is another to use home
+
+            WebElement homeLink = driver.findElement(By.linkText("Home"));
+            //Assert.assertTrue(avatarIcon.isDisplayed());
+            homeLink.click();
+
+            //test search function
+//            WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
+//            searchField.click();
+//            searchField.sendKeys("Pluto");
+
+        }
+        finally {
+            Thread.sleep(5000);
+            //Assert.assertEquals(driver.getCurrentUrl(), url);
+            driver.quit();
+        }
+
+    }
 
 
 }
