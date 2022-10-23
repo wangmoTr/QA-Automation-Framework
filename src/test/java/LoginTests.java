fd74b93e0f4adce76f183be1fc10d451ed3bd153
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-22 23:18:20 -0700

    update stash change

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -73,6 +73,7 @@ public class LoginTests {
             driver.quit();
         }
 
+<<<<<<< Updated upstream
     }
     @Test
     public static void LoginInvalidEmailTest () throws InterruptedException {
@@ -223,6 +224,125 @@ public class LoginTests {
         Thread.sleep(5000);
         driver.quit();
 
+=======
+//    }
+//    @Test
+//    public static void LoginInvalidEmailTest () throws InterruptedException {
+//        WebDriver driver = new ChromeDriver();
+//
+//        try {
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//            String url = "https://bbb.testpro.io/#!/home";
+//            driver.get(url);
+//
+//            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+//            emailField.click();
+//            emailField.sendKeys("demopp@class.com");
+//
+//            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+//            passwordField.click();
+//            passwordField.sendKeys("te$t$tudent");
+//
+//
+//            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+//            submitButton.click();
+//            //
+//            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+//            Assert.assertTrue(avatarIcon.isDisplayed());
+//        }
+//        finally {
+//            Thread.sleep(5000);
+//            //Assert.assertEquals(driver.getCurrentUrl(), url);
+//            driver.quit();
+//        }
+//
+//    }
+//    @Test
+//    public static void registrationTest () throws InterruptedException {
+//        WebDriver driver = new ChromeDriver();
+//
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//            String url = "https://bbb.testpro.io/#!/home";
+//            driver.get(url);
+//
+//            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+//            emailField.click();
+//            emailField.sendKeys("demo@class.com");
+//
+//            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+//            passwordField.click();
+//            passwordField.sendKeys("te$t$tudent");
+//
+//            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+//            submitButton.click();
+//
+//            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+//            Assert.assertTrue(avatarIcon.isDisplayed());
+//
+//
+//
+//            WebElement songMenu = driver.findElement(By.className("songs"));
+//            songMenu.click();
+//
+//
+//
+//            Thread.sleep(5000);
+//            driver.quit();
+//
+//
+//    }
+//    @Test
+//    public static void songMenuTest () throws InterruptedException {
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        String url = "https://bbb.testpro.io/#!/home";
+//        driver.get(url);
+//
+//        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+//        emailField.click();
+//        emailField.sendKeys("demo@class.com");
+//
+//        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+//        passwordField.click();
+//        passwordField.sendKeys("te$t$tudent");
+//
+//        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+//        submitButton.click();
+//
+//        WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+//        Assert.assertTrue(avatarIcon.isDisplayed());
+//
+//
+//
+//        WebElement songMenu = driver.findElement(By.className("songs"));
+//        songMenu.click();
+//        // short way
+//        driver.findElement(song).click();
+//
+//
+//        Thread.sleep(5000);
+//        driver.quit();
+//    }
+//    @Test
+//    public static void HW14 () throws InterruptedException {
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        String url = "https://bbb.testpro.io";
+//        driver.get(url);
+//
+//        WebElement registrationBtn = driver.findElement(By.id("hel"));
+//        registrationBtn.click();
+//        Thread.sleep(5000);
+//        driver.quit();
+//
+//
+//    }
+>>>>>>> Stashed changes
 
-    }
 }
