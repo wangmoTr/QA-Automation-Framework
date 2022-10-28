6b3ec96ef43b2a83fd95eb2622bc56d1b440966f
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-27 22:51:30 -0700

    refractor login

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
 
 public class BaseTest {
 
@@ -8,6 +14,19 @@ public class BaseTest {
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
+    }
+    WebDriver driver;
+    String url;
+    @BeforeMethod
+    public void launchBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+    @AfterMethod
+    public void tearDownBrowser() {
+        driver.quit();
     }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,47 +8,36 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
-public class LoginTests {
-    WebDriver driver;
-    String url;
-    @BeforeMethod
-    public void launchBrowser() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        url = "https://bbb.testpro.io/";
-        driver.get(url);
+public class LoginTests extends BaseTest{
+
+    private void providePassword(String pass) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys(pass);
     }
-    @AfterMethod
-    public void tearDownBrowser() {
-        driver.quit();
+
+    private void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys(email);
+    }
+    private void clickSubmitBtn() {
+        WebElement clickBtn = driver.findElement(By.cssSelector("[type='submit']"));
+        clickBtn.click();
+
     }
 
     @Test
     public void LoginEmptyEmailPasswordTest ()  {
              Assert.assertEquals(driver.getCurrentUrl(), url);
              driver.quit();
-
     }
 
     @Test
     public void LoginValidEmailValidPasswordTest ()  {
-//        WebDriver driver;
-//        driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        String url = "https://bbb.testpro.io/";
-//        driver.get(url);
-
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys("trangoishi99@gmail.com");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
-        passwordField.sendKeys("te$t$tudent");
-
-        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
-        submitButton.click();
+        provideEmail();
+        providePassword();
+        clickSubmitBtn();
 
         WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
         Assert.assertTrue(avatarIcon.isDisplayed());
@@ -56,22 +45,13 @@ public class LoginTests {
         driver.quit();
     }
 
+
+
     @Test
     public void LoginInvalidEmailPasswordTest () throws InterruptedException {
-//        WebDriver driver;
-//        driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        String url = "https://bbb.testpro.io/";
-//        driver.get(url);
-
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys("dem@class.com");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
-        passwordField.sendKeys("te$t$tuden");
+        provideEmail();
+        providePassword();
+        clickSubmitBtn();
 
         WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
         submitButton.click();
@@ -85,48 +65,22 @@ public class LoginTests {
 
     @Test(enabled = false)
     public void LoginValidEmailEmptyPasswordTest () {
-        WebDriver driver;
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys("demo@class.com");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
-
-        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
-        submitButton.click();
-
+        provideEmail();
+        providePassword();
+        clickSubmitBtn();
 
         driver.quit();
     }
 
     @Test(enabled = false)
     public void SwitchToSongsMenu () throws InterruptedException {
-        WebDriver driver;
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        By emailSelector = By.cssSelector("[type='email']");
-        WebElement emailField = driver.findElement(emailSelector);
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+
         By avatarSelector = By.cssSelector("[alt='Avatar of student']");
         By songMenuSelector = By.className("songs");
         By allButtonSelector = By.className("btn-shuffle-all");
 
-        emailField.click();
-        emailField.sendKeys("trangoishi99@gmail.com");
-        passwordField.click();
-        passwordField.sendKeys("te$t$tudent");
-        submitButton.click();
         Assert.assertTrue(driver.findElement(avatarSelector).isDisplayed());
         // WebElement songsMenu = driver.findElement(By.className("songs"));
         // WebElement songsMenu = driver.findElement(songMenuSelector);
