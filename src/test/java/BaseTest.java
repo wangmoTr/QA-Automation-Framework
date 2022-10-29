cc6a256b46e4dddba293b3e8ae83f7e41ac99ed5
Stan Melamed <115844713+stanTestPro@users.noreply.github.com>
2022-10-28 20:14:35 -0400

    Add Network Throttling ability

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumNetworkConditions;
+import org.openqa.selenium.chromium.HasNetworkConditions;
+import org.openqa.selenium.remote.Augmenter;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -16,18 +19,35 @@ public class BaseTest {
         // This is for Windows users
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "Untitled/chromedriver");
         }
+
+
     }
 
     @BeforeMethod
+    // Send a parameter for 'baseURL' specified in XML
     @Parameters({"baseURL"})
+    // Make baseURL parameter optional, if it is null, then set it to something)
     public void launchBrowser(@Optional String baseURL) {
         if (baseURL == null)
             baseURL ="https://bbb.testpro.io";
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // Make webdriver load the pages REALLY slow
+        WebDriver augmentedDriver = new Augmenter().augment(driver);
+        ChromiumNetworkConditions networkConditions = new ChromiumNetworkConditions();
+        networkConditions.setDownloadThroughput(100 * 1024);
+        networkConditions.setUploadThroughput(500 * 1024);
+        networkConditions.setLatency(Duration.ofMillis(5));
+        ((HasNetworkConditions) augmentedDriver).setNetworkConditions(networkConditions);
+        // (comment out above lines to remove throttling)
+
+        // Wait for an element to show up for max of X seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         url = baseURL;
         driver.get(url);
+
     }
 
     @AfterMethod
