1dfe9789c7f6dfc636293cc94922f1f397066e9d
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-29 19:55:32 -0700

    create hw17_t class

---
+++ src/test/java/hw17_t.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class hw17_t extends BaseTest{
+
+}
