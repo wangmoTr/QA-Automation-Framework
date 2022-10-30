2e86f13046463f0f2076762cb469290c15ef4f58
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-10-29 20:10:07 -0700

    test with login and pass

--- src/test/java/hw17_t.java
+++ src/test/java/hw17_t.java
@@ -9,5 +9,20 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class hw17_t extends BaseTest{
+    @Test
+    public void addSongToPlaylist() {
+        provideEmail("trangoishi99@gmail.com");
+        providePassword("te$t$tudent");
+        clickSubmitBtn();
+
+        WebElement avatarIcon = driver.findElement(By.xpath("//img[contains(@alt,'Avatar of')]"));
+        //WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        //select the all song
+        WebElement allsongs = driver.findElement(By.xpath("//a[@data-testid=\"view-profile-link\"]"));
+        allsongs.click();
+        driver.quit();
+    }
 
 }
