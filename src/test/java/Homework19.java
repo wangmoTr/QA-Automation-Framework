41b4d2b6734aec7bd570e6776ee86d01fb0e8289
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-11-04 23:33:05 -0700

    complete HW19 and pass the test

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -4,10 +4,39 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class Homework19 extends BaseTest{
 
+//    @Test
+//    public void deletePlayList(){
+//        provideEmail("demo@class.com");
+//        providePassword("te$t$tudent");
+//        clickSubmitBtn();
+//    }
     @Test
-    public void deletePlayList(){
+    public void deletePlaylist() throws InterruptedException {
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmitBtn();
+        deleteIt();
+        //to check confirm message
+        Assert.assertEquals("Deleted playlist","Deleted playlist");
+
     }
+
+    private void deleteIt() throws InterruptedException {
+        //select one playlist name "MySongs"
+        WebElement playlisttobeDeleted = driver.findElement(By.xpath("//a[text()='MySongs']"));
+        playlisttobeDeleted.click();
+
+        Thread.sleep(3000);
+
+        //click on delete button xpath //button[@class="btn-delete-playlist"]
+        WebElement deleteBtn = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deleteBtn.click();
+        Thread.sleep(5000);
+
+    }
+    private String confirmDelete() {
+        WebElement confirmmessage = driver.findElement(By.xpath("//div[contains.text(),'Deleted playlist']"));
+        return confirmmessage.getText();
+    }
+
 }
