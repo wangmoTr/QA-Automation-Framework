0a2bdcfae42b95285405595e42ded11474b68c4c
wangmoTr <83141004+wangmoTr@users.noreply.github.com>
2022-12-05 23:41:56 -0800

    Revert "Grid trang"

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class RegistrationTests extends BaseTest {
+public class RegistrationTests {
 
     @Test
     public static void RegistrationSuccessTest () {
@@ -16,5 +16,6 @@ public class RegistrationTests extends BaseTest {
         String url = "https://bbb.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
     }
 }
