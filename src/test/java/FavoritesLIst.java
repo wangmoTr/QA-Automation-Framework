3e08e67dbd42ee6b3cb0342bb5be93b65b8b6654
wangmoTr <trangoishi99@gmail.com>
2022-12-06 22:52:46 -0800

    update favlist

--- src/test/java/FavoritesLIst.java
+++ src/test/java/FavoritesLIst.java
@@ -3,6 +3,7 @@ import POM.pages.BasePage;
 import POM.pages.HomePage;
 import POM.pages.LoginPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,66 +12,68 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class FavoritesLIst extends BaseTestIntern {
-    @Test
-    public void addingFavoriteSong () {
-//        LoginPage loginPage = new LoginPage(driver);
-//        HomePage homePage = new HomePage(driver);
+    @Test(enabled = true)
+    public void addingFavoriteSong() {
 
-//        loginPage.provideEmail("trangoishi99@gmail.com");
-//        loginPage.providePassword("te$t$tudent");
-//        loginPage.clickSubmitBtn();
-//        Assert.assertTrue(homePage.isUserAvatarDisplayed());
-          login();
-          addingSongtoFavorites();
+        login();
+        addingSongtoFavorites();
 
         checkFavoriteList();
         //getFavSongTitle();
 
 
     }
-   private String getFavSongTitle() {
-       WebElement superPlaylistSong = driver.findElement(By.xpath("//section[@id='playlistWrapper']//tr[@class='song-item']//td[@class='title']"));
-       return superPlaylistSong.getText();
-   }
-   private void addingSongtoFavorites() {
-       WebElement AllSongsPage = driver.findElement(By.cssSelector(".songs"));
-       AllSongsPage.click();
 
-       WebElement favoringSong = driver.findElement(By.xpath("//section[@id='songsWrapper']//tr[@class='song-item']//td[@class='favorite']"));
-       favoringSong.click();
-   }
-   private void checkFavoriteList() {
-       WebElement favList = driver.findElement(By.xpath("//a[@href='#!/favorites']"));
-       favList.click();
-   }
+    private String getFavSongTitle() {
+        WebElement superPlaylistSong = driver.findElement(By.xpath("//section[@id='playlistWrapper']//tr[@class='song-item']//td[@class='title']"));
+        return superPlaylistSong.getText();
+    }
 
-   @Test
+    private void addingSongtoFavorites() {
+        WebElement AllSongsPage = driver.findElement(By.cssSelector(".songs"));
+        AllSongsPage.click();
+
+        WebElement favoringSong = driver.findElement(By.xpath("//section[@id='songsWrapper']//tr[@class='song-item']//td[@class='favorite']"));
+        favoringSong.click();
+    }
+
+    private void checkFavoriteList() {
+        WebElement favList = driver.findElement(By.xpath("//a[@href='#!/favorites']"));
+        favList.click();
+    }
+
+    @Test(enabled = true)
     public void deleteFavSongList() {
         login();
-       checkFavoriteList();
-//       //WebElement favList = driver.findElement(By.xpath("//section[@id='favoritesWrapper'']//i[@class='fa fa-heart text-maroon']"));
-//       //i[@data-test='btn-like-liked']
-       WebElement favList = driver.findElement(By.xpath("//section[@id='favoritesWrapper']//td[@class='title']"));
-       //WebElement favList = driver.findElement(By.xpath("//i[@data-test='btn-like-liked']"));
-       actions.doubleClick(favList).perform();
-//
-//       //find the heart symbol and unclick
-//       WebElement songPlay = driver.findElement(By.xpath("//div[@class='wrapper']//i[@class='fa fa-heart text-maroon']"));
-//       actions.doubleClick(songPlay).perform();
+        checkFavoriteList();
+        WebElement favList = driver.findElement(By.xpath("//section[@id='favoritesWrapper']//td[@class='title']"));
+        //WebElement favList = driver.findElement(By.xpath("//i[@data-test='btn-like-liked']"));
+        actions.sendKeys(Keys.DELETE);
 
-       WebElement songPlay = driver.findElement(By.xpath("//tr[@class='song-item selected']//td[@class='favorite']//i[@data-test='btn-like-unliked']"));
-         actions.doubleClick(songPlay).perform();
 
     }
-    @Test
-   public void emptyFavList() {
+
+    @Test(enabled = true)
+    public void emptyFavList() {
         login();
         checkFavoriteList();
-       // WebElement textStatusofEmptyList = driver.findElement(By.xpath("//a"))
-   }
+
+        String message = noFavorites();
+        
+        Assert.assertEquals(message, "No favorites yet.");
+    }
+
+    private String noFavorites() {
+        WebElement textStatusofEmptyList = driver.findElement(By.xpath("//section[@id='favoritesWrapper']//div[@class='text']"));
+        return textStatusofEmptyList.getText();
+    }
 
 }
+
+
