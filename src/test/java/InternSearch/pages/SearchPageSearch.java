b4b6686ba6925d36304f87d4ff4ad583d7c2107d
wangmoTr <trangoishi99@gmail.com>
2022-12-19 23:52:31 -0800

    search song method

--- src/test/java/InternSearch/pages/SearchPageSearch.java
+++ src/test/java/InternSearch/pages/SearchPageSearch.java
@@ -11,9 +11,18 @@ public class SearchPageSearch extends BasePageSearch {
     //Locators
     @FindBy(css="[type='search']")
     WebElement searchInput;
+
     //By searchInput = By.cssSelector("[type='search']");
+
+
     public SearchPageSearch(WebDriver givenDriver) {
         super(givenDriver);
+    }
+
+    public SearchPageSearch searchBox(String songName){
+        searchInput.sendKeys(songName);
+        return this;
+
     }
 
 
--- src/test/java/Search.java
+++ src/test/java/Search.java
@@ -2,12 +2,13 @@ import InternSearch.pages.LoginPageSearch;
 import InternSearch.pages.SearchPageSearch;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class Search extends BaseTest {
-    By songTextFromAllSongsList= By.xpath("//section[@id='songsWrapper']//tr[@class='song-item selected']//td[@class='title']");
+    String songName = "Dark Days";
     @Test(enabled = true)// dataProvider = "songTitles", dataProviderClass = BaseTest.class)
     public void searchSongTitle() throws InterruptedException{
 
@@ -15,23 +16,19 @@ public class Search extends BaseTest {
         LoginPageSearch Login = new LoginPageSearch(driver);
         SearchPageSearch searching = new SearchPageSearch(driver);
         Login.login();
-        songTitleInfofromAllSong();
-//        searching.songTitleInfofromAllSong();
-        //Assert.assertEquals(searching.songTitleInfofromAllSong(),true);
-
-//        WebElement searchInput = driver.findElement(By.cssSelector("[type='search']"));
-//        searchInput.sendKeys("Dark Days");
-
+        searching.searchBox(songName);
 
     }
      //expect to see result
      public void resultFromSearch(){
+         //*[@id='searchExcerptsWrapper']//span[text()="Dark Days"]
         
      }
-     public void songTitleInfofromAllSong() {
-        WebElement songText= driver.findElement(By.xpath("//section[@id='songsWrapper']//tr[@class='song-item selected']//td[@class='title']"));
-        Assert.assertEquals("song","song");
-        //Assert.assertEquals(songText.getText(),"Dark Days");
+     public boolean isSongExisting(String songName) {
+         WebElement allsongs = driver.findElement(By.xpath("//a[@href='#!/songs']"));
+         allsongs.click();
+        WebElement songText= driver.findElement(By.xpath("//*[@id='songsWrapper']//td[text()='"+ songName+"']"));
+        return songText.isDisplayed();
      }
 
 
