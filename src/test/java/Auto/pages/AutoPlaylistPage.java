a063152e66ea5d2f2943621043ba151ab7e17202
wangmoTr <trangoishi99@gmail.com>
2022-12-26 15:17:27 -0800

    testing on how to add new list

--- src/test/java/Auto/pages/AutoPlaylistPage.java
+++ src/test/java/Auto/pages/AutoPlaylistPage.java
@@ -8,18 +8,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class AutoPlaylistPage extends AutoBasePage {
 
     //Locators
-      //By addPlaylistBtn =By.xpath("//*[@id='playlists']//*[@data-testid='sidebar-create-playlist-btn']");
-    By addPlaylistBtn =By.xpath("//*[@id='playlists']//*[@data-testid='sidebar-create-playlist-btn']//li[@data-testid='playlist-context-menu-create-simple']");
+      By playlistLocator =By.xpath("//*[@id='playlists']//*[@data-testid='sidebar-create-playlist-btn']");
+      By addingPlaylistBtn =By.xpath("//*[@id='playlists']//*[@data-testid='sidebar-create-playlist-btn']//li[@data-testid='playlist-context-menu-create-simple']");
+   ////li[contains(text(),'New Playlist']
+       //By addingPlaylistBtn = By.xpath("//div[contains(text(),'New Playlist']");
 
-    String [] nameofList = { "abc", "abc", "abcd", "abcdef", "tencharact","eleveneCharcter"};
+    By enterNameList = By.cssSelector("[type ='text]");
+
+
 
     public AutoPlaylistPage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
-    public AutoPlaylistPage createListPage() {
-        driver.findElement(addPlaylistBtn).click();
+    public AutoPlaylistPage playListArea() {
+        driver.findElement(playlistLocator).click();
         return this;
+    }
+
+    public void addingPlaylistbtn() {
+        driver.findElement(addingPlaylistBtn).click();
+
+    }
+
+    public AutoPlaylistPage inputPlayListName(String playlistName) {
+      driver.findElement(enterNameList).sendKeys(playlistName);
+      return this;
     }
 
 //    public AutoPlaylistPage shuffle(){
--- src/test/java/CreatePlayList.java
+++ src/test/java/CreatePlayList.java
@@ -11,6 +11,8 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class CreatePlayList extends BaseTest{
+
+    String [] nameofList = { "abc", "abc", "abcd", "abcdef", "tencharact","eleveneCharcter"};
     @Test
     public void createNewList () {
         AutoLoginPage loginPage = new AutoLoginPage(driver);
@@ -18,8 +20,15 @@ public class CreatePlayList extends BaseTest{
         AutoPlaylistPage playList = new AutoPlaylistPage(driver);
         loginPage.login();
         Assert.assertTrue(homePage.isUserAvatarDisplayed());
+        playList.playListArea();
+        playList.addingPlaylistbtn();
+        //playList.inputPlayListName("abc");
+        //Assert.assertEquals(getConfirmationPopupText(), "Created playlist abc");
 
     }
 
     ////*[@id='playlists']//*[@data-testid='sidebar-create-playlist-btn']//li[@data-testid='playlist-context-menu-create-simple']
+    private String getConfirmationPopupText() {
+        return driver.findElement(By.cssSelector("div.success.show")).getText();
+    }
 }
