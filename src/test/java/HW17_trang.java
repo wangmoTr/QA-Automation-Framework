dd53b4792ef6e6473e83af5abaf3707f20a16798
wangmoTr <trangoishi99@gmail.com>
2023-02-07 23:35:31 -0800

    add BaseTestSimple

--- src/test/java/HW17_trang.java
+++ src/test/java/HW17_trang.java
@@ -1,2 +1,7 @@
-public class HW17_trang {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HW17_trang extends BaseTestSimple {
 }
