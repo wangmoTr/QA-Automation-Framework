fbd04169a614b5f4f4fd6559a4a549c780507ced
wangmoTr <trangoishi99@gmail.com>
2023-02-08 00:09:47 -0800

    update

--- src/test/java/HW17_trang.java
+++ src/test/java/HW17_trang.java
@@ -3,5 +3,14 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HW17_trang extends BaseTestSimple {
+public class HW17_trang extends SimpleBaseTest {
+
+    @Test
+    public static void addSongToPlayList() {
+
+    }
+
+    public void login() {
+
+    }
 }
