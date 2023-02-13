d3c9aabd079d2951d9683fe0d7cb841c24664f6f
wangmoTr <trangoishi99@gmail.com>
2023-02-12 23:55:43 -0800

    all songs locator

--- src/test/java/HW17_trang.java
+++ src/test/java/HW17_trang.java
@@ -8,9 +8,20 @@ public class HW17_trang extends SimpleBaseTest {
     @Test
     public void addSongToPlayList() {
         login();
-        WebElement avatarIcon = driver.findElement(By.cssSelector("alt='Avatar of student']"));
-        //Assert.assertTrue(avatarIcon.isDisplayed());
+        WebElement avatarIcon = driver.findElement(By.xpath("//*[@alt='Avatar of t']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        WebElement  allSongs = driver.findElement(By.xpath("//*[@href='#!/songs']"));
+        allSongs.click();
+        //td
+        //BossStatus
+
+        WebElement  selectedSong = driver.findElement(By.xpath("//*[contains(text(),'BossStatus']"));
+        selectedSong.click();
+       //  //span[contains(.,'DarkDays')
     }
 
-
 }
+
+//        WebElement  listtoAdd = driver.findElement(By.xpath("//a[contains(text(), 'list1')]"));
+//        listtoAdd.click();
